75641ca6d3d33a892715af589101461e82edc1e3
Gidong Kwon <lucidfext@gmail.com>
2018-02-27 19:35:10 +0900

    Refactor class name

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Route } from 'react-router-dom';
 import Home from "./pages/Home";
-import OthelloGame from "./pages/OthelloGame";
+import PlayOthello from "./pages/PlayOthello";
 
 export default class App extends React.Component {
 
@@ -16,8 +16,8 @@ export default class App extends React.Component {
     render() {
         return (
             <>
-                <Route exact path='/' component={Home} />
-                <Route path='/othello' component={OthelloGame} />
+                <Route exact path='/' component={ Home } />
+                <Route path='/othello' component={ PlayOthello } />
             </>
         );
     }
--- src/components/pages/PlayOthello.tsx
+++ src/components/pages/PlayOthello.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-export default class OthelloGame extends React.Component {
+export default class PlayOthello extends React.Component {
     render() {
         return (
             <h1>Othello</h1>
