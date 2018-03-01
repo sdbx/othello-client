9190d7550acd4b2940bfb3727495ae9c982cc9d4
Gidong Kwon <lucidfext@gmail.com>
2018-03-01 21:37:07 +0900

    Add basic styling

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import * as styles from './Menu.scss';
 
 export default class Menu extends React.Component {
     render() {
         return (
             <>
-                <ul>
+                <ul className={styles['menu']}>
                     <li><Link to='/'>Home</Link></li>
                     <li><Link to='/othello'>Othello</Link></li>
                 </ul>
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Root from './components/Root';
+import './index.scss';
 
 ReactDOM.render(<Root />, document.getElementById('app'));
